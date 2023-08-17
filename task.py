519645de349edb5b3a1e42f2474c63f6fddce692
senthilkumar123456 <141622183+senthilkumar123456@users.noreply.github.com>
2023-08-17 12:12:22 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,24 @@
+##task:1
+##num=int(input('enter your number:'))
+##num +=3
+##num *=2
+##print('the result is num',num)
+##task:2
+##p=6
+##q=8
+##print(p^q)
+##print(~p)
+##task:3
+##is_sunny=False
+##is_warm=True
+##is_sunny_and_warm=is_sunny and is_warm
+##print('is it sunny and warm?',is_sunny_and_warm)
+##task:4
+##name=str(input('enter the name'))
+##age=str(input('enter the age'))
+##print('my name is',name,'and i am',age,'years old.')
+##task:5
+##even=int(input('enter the num of even'))
+##odd=int(input('enter the num of odd'))
+##num=even%odd
+##print('indicating whether num is',num) 
